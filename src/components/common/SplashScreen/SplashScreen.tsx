e8b26555c31b0eda67fa2e18de0f7710c9fc494e
Canberkkmn <canberkkaman@gmail.com>
2023-08-27 18:00:19 +0300

    remove not necessary codes

--- src/components/common/SplashScreen/SplashScreen.tsx
+++ src/components/common/SplashScreen/SplashScreen.tsx
@@ -1,21 +1,13 @@
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
 
 const SplashScreen: FC = () => {
-  const [open, setOpen] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setOpen(false);
-    }, 5000);
-  }, []);
-
   return (
     <div>
       <Backdrop
         sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={open}
+        open
       >
         <CircularProgress color="inherit" />
       </Backdrop>
